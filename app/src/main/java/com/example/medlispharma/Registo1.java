dd8dea31c43625eb4ebc2a470141c391fe38e0e9
Sara <saravdp@live.com.pt>
2021-04-11 01:49:57 +0100

    updates

--- app/src/main/java/com/example/medlispharma/Registo1.java
+++ app/src/main/java/com/example/medlispharma/Registo1.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,12 +16,18 @@ public class Registo1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registo1);
         final Button writeTagButton = findViewById(R.id.button);
-
+        EditText nomeMed = (EditText) findViewById(R.id.NomeMedicamento);
+        EditText validadeMed = (EditText) findViewById(R.id.DataValidade);
+        EditText nDoses = (EditText) findViewById(R.id.NDoses);
         writeTagButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent q1 = new Intent(Registo1.this, Registo2.class);
+                q1.putExtra("nomeMed", nomeMed.getText().toString());
+                q1.putExtra("validadeMed", validadeMed.getText().toString());
+                q1.putExtra("nDoses", nDoses.getText().toString());
                 startActivity(q1);
             }
         });
+
     }
 }
--- app/src/main/java/com/example/medlispharma/TagRead.java
+++ app/src/main/java/com/example/medlispharma/TagRead.java
@@ -2,7 +2,11 @@ package com.example.medlispharma;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class TagRead extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class TagRead extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tag_read);
+
+        int timeout = 2000; // make the activity visible for 4 seconds
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+
+            @Override
+            public void run() {
+                finish();
+                Bundle extras = getIntent().getExtras();
+                if (extras != null) {
+                    //The key argument here must match that used in the other activity
+                    Intent medication = new Intent(TagRead.this, Menu.class);
+                    startActivity(medication);
+                }
+            }
+        }, timeout);
     }
 }
